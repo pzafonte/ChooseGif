2138edbaa39bedc7f4fab3ba9eccb5756d620155
Peter David Zafonte <pzafonte@users.noreply.github.com>
2018-09-25 14:06:33 -0400

    working code without any styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,134 @@
+var tags = ["beer", "coffee"];
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=";
+var numTags = tags.length;
 
-    // Adding click event listen listener to all buttons
-    $("button").on("click", function() {
-      // Grabbing and storing the data-animal property value from the button
-      var animal = $(this).attr("data-animal");
 
-      // Constructing a queryURL using the animal name
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&api_key=d5mc8D0xM3CQkTbaHTEfFttCoDD9RVam";
+//Toggle Between Still and Animated Gifs 
+function toggleGIFState() {
 
-      // Performing an AJAX request with the queryURL
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        // After data comes back from the request
-        .then(function(response) {
-          console.log(queryURL);
+  console.log("entering toggle gif state");
 
-          console.log(response);
-          // storing the data from the AJAX request in the results variable
-          var results = response.data;
+  var imagePath = $("img", this).attr("src");
 
-          // Looping through each result item
-          for (var i = 0; i < results.length; i++) {
+  console.log(imagePath);
 
-            // Creating and storing a div tag
-            var animalDiv = $("<div>");
+  if (imagePath.includes("_s")) {
+    imagePath = imagePath.replace("_s.gif", ".gif");
+  } else {
+    imagePath = imagePath.replace(".gif", "_s.gif");
+  }
+  console.log($("img", this).attr("src", imagePath));
+  $("img", this).attr("src", imagePath);
 
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + results[i].rating);
+}
 
-            // Creating and storing an image tag
-            var animalImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
+//Pull Gifs from GIPHY API and Render to the DOM
+function getGIFs(query) {
+  $.ajax({
+      url: queryURL + query,
+      method: "GET"
+    })
+    // After data comes back from the request
+    .then(function (response) {
+      console.log(queryURL);
 
-            // Appending the paragraph and image tag to the animalDiv
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
+      console.log(response);
+      // storing the data from the AJAX request in the results variable
+      var results = response.data;
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#gifs-appear-here").prepend(animalDiv);
-          }
-        });
-    });
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
 
-    $(".gif").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
+        // Creating and storing a div tag
+        var gifDiv = $("<div>").addClass("gif");
+
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        // Creating and storing an image tag
+        var gifImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+
+        // Appending the paragraph and image tag to the animalDiv
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gifs-appear-here").prepend(gifDiv);
       }
     });
+}
+//Generate gif tags based on array and perform search
+function updateAppState(query) {  
+  
+  //Initial App State
+  if (arguments.length == 0){
+    for (var i = 0; i < numTags; i++) {
+      var buttonDiv = $("<div>");
+      var buttonEl = $("<button>").text(tags[i]);
+      buttonDiv.append(buttonEl);
+      $("#buttons-go-here").append(buttonDiv);
+    }
+  }
+  else if (arguments.length == 1)
+  {
+
+    //Add Tag Array if search Query  is not already in the array of tags and then display.
+    if (tags.indexOf(query) === -1) {
+      tags.push(query);
+      numTags++;
+      console.log(query);
+      $("#buttons-go-here").append("<div><button>"+query+"</button></div>");
+      getGIFs(query);
+    } else {
+      //It already exists in our array. Just show the Gifs!
+  
+      getGIFs(query);
+    }
+
+  }
+
+    $("button").on("click", function() {
+      getGIFs($(this).text());
+  });
+
+
+  }
+  
+  
+
+
+
+
+
+
+
+$(document).ready(function () {
+  updateAppState();
+
+
+
+  $("button").on("click", function () {
+    event.preventDefault(); 
+
+    var searchTerm = $(this).text();
+    getGIFs(searchTerm);
+    updateAppState(searchTerm);
+
+
+  });
+
+  $("#get-gifs").on("click", function () {
+    event.preventDefault(); 
+
+    var searchTerm = $("#search-text").val();
+    updateAppState(searchTerm);
+
+  });
+
+
+
+});
+
+$("body").on("click", ".gif", toggleGIFState);
